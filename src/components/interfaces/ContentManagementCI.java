dd9eabbe0d06cec877e75adb188740175c31df95
waradia <aboubacardiawara909@gmail.com>
2023-02-20 01:25:27 +0100

    adding some comment to classes

--- src/components/interfaces/ContentManagementCI.java
+++ src/components/interfaces/ContentManagementCI.java
@@ -7,10 +7,36 @@ import fr.sorbonne_u.components.interfaces.RequiredCI;
 import interfaces.ContentDescriptorI;
 import interfaces.ContentTemplateI;
 
-public interface ContentManagementCI 
-extends RequiredCI, OfferedCI{
-  ContentDescriptorI find(ContentTemplateI cd, int hops) throws Exception;
-  Set<ContentDescriptorI> match(ContentTemplateI cd, 
-    Set<ContentDescriptorI> matched, 
-    int hops) throws Exception;
+/**
+ * The interface <code>ContentManagementCI</code> is offered and required by
+ * both facades and node components to allow contents search.
+ * 
+ * @author ABSSI (Team)
+ *
+ */
+public interface ContentManagementCI extends RequiredCI, OfferedCI {
+
+	/**
+	 * Takes as parameters a content pattern and a maximum number of authorized
+	 * jumps between peers to return the descriptor of the content matching the
+	 * pattern found by browsing the peers
+	 * 
+	 * @param cd   : the partial description of the content to find.
+	 * @param hops : the maximum number of jumps between nodes.
+	 * @return the complete description of the content or null if it not found
+	 * @throws Exception
+	 */
+	ContentDescriptorI find(ContentTemplateI cd, int hops) throws Exception;
+
+	/**
+	 * Serch all in the network all the contents which match with the giving
+	 * description.
+	 * 
+	 * @param cd : a partial content description.
+	 * @param matched : a set of content descriptors already found
+	 * @param hops : maximum number of jumps allowed between peers 
+	 * @return a set of content complete descriptors matched.
+	 * @throws Exception
+	 */
+	Set<ContentDescriptorI> match(ContentTemplateI cd, Set<ContentDescriptorI> matched, int hops) throws Exception;
 }
--- src/components/interfaces/NodeManagementCI.java
+++ src/components/interfaces/NodeManagementCI.java
@@ -5,9 +5,41 @@ import java.util.Set;
 import fr.sorbonne_u.components.interfaces.OfferedCI;
 import fr.sorbonne_u.components.interfaces.RequiredCI;
 import interfaces.PeerNodeAddressI;
-public interface NodeManagementCI 
-extends OfferedCI, RequiredCI {
 
-  Set<PeerNodeAddressI> join(PeerNodeAddressI a) throws Exception;
-  void leave(PeerNodeAddressI a) throws Exception;
+/**
+ * The interface <code>NodeManagementCI</code> represents an interface which is
+ * offered by facade components and required by node components.
+ * <p>
+ * <strong>Description</strong>
+ * </p>
+ * 
+ * <p>
+ * It most general way, it allows to a node to join or leave the network.
+ * </p>
+ * 
+ * @author ABSSI (Team)
+ *
+ */
+public interface NodeManagementCI extends OfferedCI, RequiredCI {
+
+	/**
+	 * Takes as a parameter the address of the peer node to be inserted into the
+	 * network and returns a set of peer node addresses to which it can connect as
+	 * new neighbor
+	 * 
+	 * @param a : address of the peer node which want to join the network.
+	 * @return
+	 * @throws Exception
+	 */
+	Set<PeerNodeAddressI> join(PeerNodeAddressI a) throws Exception;
+
+	/**
+	 * Takes as a parameter the address of the peer node leaving the network.
+	 * Components playing the role of peer nodes provide the NodeCI interface with
+	 * the declared services next
+	 * 
+	 * @param a : address of the peer node which want to join the network.
+	 * @throws Exception
+	 */
+	void leave(PeerNodeAddressI a) throws Exception;
 }
