f93afa9c183e225ce1f690d2b3dec470dfe82778
waradia <aboubacardiawara909@gmail.com>
2023-05-04 17:23:23 +0200

    enhancing tests

--- src/test/java/content/ContentDescriptorTest.java
+++ src/test/java/content/ContentDescriptorTest.java
@@ -1,22 +1,16 @@
 package test.java.content;
 
 import static org.junit.Assert.*;
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-
-import java.util.HashSet;
-import java.util.Set;
 
 import org.junit.Test;
 
-import main.java.implem.ContentDescriptor;
-import main.java.implem.ContentTemplate;
 import main.java.interfaces.ContentDescriptorI;
 import main.java.interfaces.ContentTemplateI;
 
 public class ContentDescriptorTest {
 
 	@Test
-	public void testMatch() {
+	public void testMatchBasic() {
 		ContentTemplateI contentTemplate1 = 
 				new ContentTemplateBuilder()
 				.setTitle("Titre1")
@@ -57,11 +51,10 @@ public class ContentDescriptorTest {
 		assertFalse(contentDescriptor1.match(contentTemplate2));
 		assertTrue(contentDescriptor2.match(contentTemplate2));
 		assertFalse(contentDescriptor2.match(contentTemplate1));
-		 
 	}
 	
 	@Test
-	public void testEquals()  {
+	public void testEqualsBasic()  {
 		ContentDescriptorI contentDescriptor1a = 
 				new ContentDescriptorBuilder()
 				.setTitle("Titre1")
@@ -90,9 +83,73 @@ public class ContentDescriptorTest {
 				.setAdress(null)
 				.build();
 		
-		assertTrue(contentDescriptor1a.equals(contentDescriptor1b));
-		assertFalse(contentDescriptor1a.equals(contentDescriptor2));
-		assertFalse(contentDescriptor1b.equals(contentDescriptor2));
+		assertTrue(
+				"content 1a and 1b are equals because they have similar values in their"
+				+ " respective fields (title, album, ...)", 
+				contentDescriptor1a.equals(contentDescriptor1b)
+			);
+		assertFalse(
+				"content 1a and 2 are not equals because they doesn't have similar values in their"
+						+ " respective fields (title, album, ...)", 
+				contentDescriptor1a.equals(contentDescriptor2)
+			);
+		assertFalse(
+				"content 1b and 2 are not equals because they doesn't have similar values in their"
+						+ " respective fields (title, album, ...)", 
+				contentDescriptor1b.equals(contentDescriptor2)
+			);
+	}
+	
+	@Test
+	public void testTwoContentsWithAtLeastOneDifferentFieldAreNotEquals() {
+		ContentDescriptorI contentDescriptor1a = 
+				new ContentDescriptorBuilder()
+				.setTitle("Titre1a")
+				.setAlbum("Album1")
+				.addInterpreter("I1")
+				.addcomposer("C1")
+				.setFileSize((long) 10)
+				.setAdress(null)
+				.build();
+		
+		ContentDescriptorI contentDescriptor1b =
+				new ContentDescriptorBuilder()
+				.setTitle("Titre1b")
+				.setAlbum("Album1")
+				.addInterpreter("I1")
+				.addcomposer("C1")
+				.setFileSize((long) 10)
+				.setAdress(null)
+				.build();
+		
+		assertFalse(contentDescriptor1a.equals(contentDescriptor1b));
+		
+	}
+	
+	@Test
+	public void testTwoContentsWithAtLeastOneSimilarFieldCanMatch() {
+		ContentDescriptorI contentDescriptor1 = 
+				new ContentDescriptorBuilder()
+				.setTitle("Titre1")
+				.setAlbum("Album1")
+				.addInterpreter("I1")
+				.addcomposer("C1")
+				.setFileSize((long) 10)
+				.setAdress(null)
+				.build();
+		
+		ContentDescriptorI contentDescriptor2 =
+				new ContentDescriptorBuilder()
+				.setTitle("Titre2")
+				.setAlbum("Album2")
+				.addInterpreter("I2")
+				.addcomposer("C2")
+				.addcomposer("C1")
+				.setFileSize((long) 10)
+				.setAdress(null)
+				.build();
+		
+		assertTrue(contentDescriptor1.match(contentDescriptor2));
 	}
 
 }
